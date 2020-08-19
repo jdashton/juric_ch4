022484013d25cba9e53645e23bfbf16f832cff46
Daniel Ashton <jdashton@ashtonfam.org>
2020-08-19 17:04:52 -0400

    WIP - add_entry/2

--- lib/simple_todo.ex
+++ lib/simple_todo.ex
@@ -1,8 +1,18 @@
 defmodule TodoList do
+  defstruct auto_id: 1, entries: %{}
+
   def new() do
   end
 
   def add_entry(_, _, _) do
+  end
+
+  def add_entry(%{entries: entries, auto_id: next_id} = todo_list, entry) do
+    entry = Map.put(entry, :id, next_id)
+
+    entries = Map.put(entries, next_id, entry)
+
+    %TodoList{todo_list | auto_id: next_id + 1, entries: entries}
   end
 
   def entries(_, _) do
