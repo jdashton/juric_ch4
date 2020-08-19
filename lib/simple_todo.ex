843c3329c2252aa4742dce00ec4db4d4457a2320
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-08-19 17:21:11 -0400

    page 108

--- lib/simple_todo.ex
+++ lib/simple_todo.ex
@@ -1,20 +1,21 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
-  def new() do
+  def new(), do: MultiDict.new()
+
+  def add_entry(todo_list, entry) do
+    MultiDict.add(todo_list, entry.date, entry)
   end
 
-  def add_entry(_, _, _) do
-  end
+  # def add_entry(%{entries: entries, auto_id: next_id} = todo_list, entry) do
+  #   entry = Map.put(entry, :id, next_id)
 
-  def add_entry(%{entries: entries, auto_id: next_id} = todo_list, entry) do
-    entry = Map.put(entry, :id, next_id)
+  #   entries = Map.put(entries, next_id, entry)
 
-    entries = Map.put(entries, next_id, entry)
+  #   %TodoList{todo_list | auto_id: next_id + 1, entries: entries}
+  # end
 
-    %TodoList{todo_list | auto_id: next_id + 1, entries: entries}
-  end
-
-  def entries(_, _) do
+  def entries(todo_list, date) do
+    MultiDict.get(todo_list, date)
   end
 end
--- test/simple_todo_test.exs
+++ test/simple_todo_test.exs
@@ -6,20 +6,22 @@ defmodule TodoListTest do
     setup do
       todo_list =
         TodoList.new()
-        |> TodoList.add_entry(~D[2018-12-19], "Dentist")
-        |> TodoList.add_entry(~D[2018-12-20], "Shopping")
-        |> TodoList.add_entry(~D[2018-12-19], "Movies")
+        |> TodoList.add_entry(%{date: ~D[2018-12-19], title: "Dentist"})
+        |> TodoList.add_entry(%{date: ~D[2018-12-20], title: "Shopping"})
+        |> TodoList.add_entry(%{date: ~D[2018-12-19], title: "Movies"})
 
       %{todo_list: todo_list}
     end
 
-    @tag :pending
     test "retrieves entries for Dec 19", context do
       dec_19_entries = TodoList.entries(context.todo_list, ~D[2018-12-19])
-      assert dec_19_entries == ["Movies", "Dentist"]
+
+      assert dec_19_entries == [
+               %{date: ~D[2018-12-19], title: "Movies"},
+               %{date: ~D[2018-12-19], title: "Dentist"}
+             ]
     end
 
-    @tag :pending
     test "retrieves entries for Dec 18", context do
       dec_18_entries = TodoList.entries(context.todo_list, ~D[2018-12-18])
       assert dec_18_entries == []
